71fa9130761d0bb2b70b2f91be5fa8201d4f550d
Zack Tang <zacktang113@gmail.com>
2022-01-14 14:01:16 -0800

    update table function

--- src/straPy/data_vis.py
+++ src/straPy/data_vis.py
@@ -24,27 +24,35 @@ def density_ci_plot(dist, ci):
     """
     pass
 
-def ci_table(dist, ci):
-    """Makes a table that shows mean, lower bound and upper bound of the confidence interval
+def ci_table(dist, ci, se=True, size=True, mean=True, alpha=0.05):
+    """Makes a table that shows lower bound and upper bound of the confidence interval and relevant statistics
 
-    A table that contains the provided sampling distribution
-    `dist`'s mean and its confidence interval `ci`.
+    A table that contains the provided sampling distribution`dist`'s 
+    confidence interval `ci` and other relevant statistics, including
+    standard error `se`, sample size `size`, sample mean `mean` and 
+    selected alpha `alpha`.
 
 
     Parameters
     ----------
-   dist : numpy.ndarray
+    dist : numpy.ndarray
         bootstrapped sampling distribution
     ci : numpy.ndarray
         confidence interval
-    
+    size : boolean, default=True
+        sample size
+    mean: boolean, default=True
+        sample mean
+    alpha : float, default=0.05
+        selected alpha
+
     Returns
     -------
     table object
-        table with mean, lower bound and upper bound of the confidence interval
+        table with lower bound and upper bound of the confidence interval and relevant statistics
     
     Examples
     --------
-    >>> ci_table([1, 1, 2, 3, 5, 10]，[1., 9.375]）
+    >>> ci_table([1, 1, 2, 3, 5, 10]，[1., 9.375], se=True, size=True, mean=True, alpha=0.05）
     """
     pass
