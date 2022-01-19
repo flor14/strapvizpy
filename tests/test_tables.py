effde2403906bdd68cb117f7e80e254b851d7d40
voremargot <margot.vore@outlook.com>
2022-01-19 13:58:39 -0800

    Initilize tests for table creation funtion

---
+++ tests/test_tables.py
@@ -0,0 +1,6 @@
+import numpy as np
+from pytest import raises
+from straPy.bootstrap import bootstrap_distribution
+from straPy.bootstrap import calculate_boot_stats
+from straPy.data_viz import summary_tables
+
