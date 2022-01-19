8a9ad04b6b25bff4d7d54ff9ab3e2e7be76f9a25
voremargot <margot.vore@outlook.com>
2022-01-19 07:37:45 -0800

    Create function for making summary tables

--- src/straPy/data_vis.py
+++ src/straPy/data_vis.py
@@ -24,35 +24,84 @@ def density_ci_plot(dist, ci):
     """
     pass
 
-def ci_table(dist, ci, se=True, size=True, mean=True, alpha=0.05):
-    """Makes a table that shows lower bound and upper bound of the confidence interval and relevant statistics
 
-    A table that contains the provided sampling distribution`dist`'s 
-    confidence interval `ci` and other relevant statistics, including
-    standard error `se`, sample size `size`, sample mean `mean` and 
-    selected alpha `alpha`.
+def ci_table(stat_dic, precision=2, estimator=True, alpha=True):
+    """Makes two tables that summerize the statistics from the bootstrapped samples and the parameters for creating the bootstrapped samples.
 
 
     Parameters
     ----------
-    dist : numpy.ndarray
-        bootstrapped sampling distribution
-    ci : numpy.ndarray
-        confidence interval
-    size : boolean, default=True
-        sample size
-    mean: boolean, default=True
-        sample mean
-    alpha : float, default=0.05
-        selected alpha
+    stat_dic : dict
+        summary statistics produced by the `calculate_boot_stats()` function 
+    precision : int, default= 2
+        the precision of the table values
+    esitmator : boolean, default=True
+        include the bootstrap estimate in the summary statistics table
+    alpha: boolean, default=True
+        include the significance level in the summary statistics table
 
     Returns
     -------
-    table object
-        table with lower bound and upper bound of the confidence interval and relevant statistics
+    summary statistics: table object
+        table summerizing the lower bound and upper bound of the confidence interval, the standard error, the sampling statitic (if estimator = True), and the significance level (if alpha = True)
+    bootstrap parameters: table object
+        table  summerizing the parameters of the bootstrap sampling spficiying the original sample size, number of repititions, the significance level, and the number of samples in each bootstrap if its different from the original sample size.  
     
     Examples
     --------
-    >>> ci_table([1, 1, 2, 3, 5, 10]，[1., 9.375], se=True, size=True, mean=True, alpha=0.05）
+    >>> st = calculate_boot_stats([1, 2, 3, 4], 1000, level=0.95, random_seed=123)
+    >>> stats_table, parameter_table  = ci_table(st)
+    >>> stats_table
+    >>> parameter_table
     """
-    pass
+    
+    # define the statistics table
+    df = pd.DataFrame(data=np.array([(stat_dic["lower"], stat_dic["upper"], stat_dic["std_err"])]),
+        columns=["Lower bound CI", "Upper bound CI", "Standard Error"])
+
+    if estimator is True:
+        s_name = "Sample " + stat_dic["estimator"]
+        df[s_name] = np.round(stat_dic["sample_" + stat_dic["estimator"]], rounding)
+
+    if alpha is True:
+        df["Significance Level"] = 1 - stat_dic["level"]
+        stats_table = df.style.format(
+            precision=precision, formatter={("Significance Level"): "{:.3f}"}
+        )
+    else:
+        stats_table = df.style.format(precision=rounding)
+
+    #set formatting and caption for table
+    stats_table.set_caption(
+        "Bootstrapping sample statistics from sample with "
+        + str(st["sample_size"])
+        + " records"
+    ).set_table_styles(
+        [{"selector": "caption", "props": "caption-side: bottom; font-size:1.00em;"}],
+        overwrite=False,
+    )
+
+    df_bs = pd.DataFrame(
+        data=np.array(
+            [
+                (
+                    round(stat_dic["sample_size"], 0),
+                    round(stat_dic["rep"], 0),
+                    1 - stat_dic["level"],
+                )
+            ]
+        ),
+        columns=["Sample Size", "Repetition", "Significance Level"],
+    )
+    if stat_dic["n"] != "auto":
+        df_bs["Samples per bootstrap"] = round(stat_dic["n"], 0)
+
+    bs_params = df_bs.style.format(
+        precision=0, formatter={("Significance Level"): "{:.3f}"}
+    )
+    bs_params.set_caption("Parameters used for bootstrapping").set_table_styles(
+        [{"selector": "caption", "props": "caption-side: bottom; font-size:1.00em;"}],
+        overwrite=False,
+    )
+
+    return stats_table, bs_params
