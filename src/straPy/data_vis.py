fbe2702290f1442cdd5b0692d20ada86c3985804
voremargot <margot.vore@outlook.com>
2022-01-20 06:00:02 -0800

    updated name of input parameter

--- src/straPy/data_vis.py
+++ src/straPy/data_vis.py
@@ -57,13 +57,13 @@ def histogram_ci_plot(sample, rep, bin_size = 30, n="auto", ci_level=0.95, ci_ra
     plt.ylabel(y_axis)
 
 
-def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
+def summary_tables(stat, precision=2, estimator=True, alpha=True):
     """Makes two tables that summerize the statistics from the bootstrapped samples and the parameters for creating the bootstrapped samples.
 
 
     Parameters
     ----------
-    stat_dic : dict
+    stat : dict
         summary statistics produced by the `calculate_boot_stats()` function 
     precision : int, default=2
         the precision of the table values
@@ -75,10 +75,15 @@ def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
     Returns
     -------
     summary statistics: table object
-        table summerizing the lower bound and upper bound of the confidence interval, the standard error, the sampling statitic (if estimator = True), and the significance level (if alpha = True)
+        table summerizing the lower bound and upper bound of the confidence interval,
+        the standard error, the sampling statitic (if estimator = True), and the
+        significance level (if alpha = True)
     bootstrap parameters: table object
-        table  summerizing the parameters of the bootstrap sampling spficiying the original sample size, number of repititions, the significance level, and the number of samples in each bootstrap if its different from the original sample size.  
-    
+        table  summerizing the parameters of the bootstrap sampling spficiying 
+        the original sample size, number of repititions, the significance level, 
+        and the number of samples in each bootstrap if its different from the 
+        original sample size.
+        
     Examples
     --------
     >>> st = calculate_boot_stats([1, 2, 3, 4], 1000, level=0.95, random_seed=123)
@@ -86,7 +91,7 @@ def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
     >>> stats_table
     >>> parameter_table
     """
-    if not isinstance(stat_dic, dict):
+    if not isinstance(stat, dict):
         raise TypeError("Input statistics must be a dictionary")
             
     if not isinstance(precision, int):
@@ -97,15 +102,15 @@ def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
         raise TypeError("The estimator and alpha parameter must be of type boolean.")
     
     # define the statistics table
-    df = pd.DataFrame(data=np.array([(stat_dic["lower"], stat_dic["upper"], stat_dic["std_err"])]),
+    df = pd.DataFrame(data=np.array([(stat["lower"], stat["upper"], stat["std_err"])]),
                       columns=["Lower Bound CI", "Upper Bound CI", "Standard Error"])
 
     if estimator is True:
-        s_name = "Sample " + stat_dic["estimator"]
-        df[s_name] = np.round(stat_dic["sample_" + stat_dic["estimator"]], rounding)
+        s_name = "Sample " + stat["estimator"]
+        df[s_name] = np.round(stat["sample_" + stat["estimator"]], rounding)
 
     if alpha is True:
-        df["Significance Level"] = 1 - stat_dic["level"]
+        df["Significance Level"] = 1 - stat["level"]
         stats_table = df.style.format(
             precision=precision, formatter={("Significance Level"): "{:.3f}"}
         )
@@ -115,7 +120,7 @@ def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
     # set formatting and caption for table
     stats_table.set_caption(
         "Bootstrapping sample statistics from sample with "+ 
-        str(stat_dic["sample_size"]) + " records"
+        str(stat["sample_size"]) + " records"
     ).set_table_styles(
         [{"selector": "caption", "props": "caption-side: bottom; font-size:1.00em;"}],
         overwrite=False)
@@ -123,11 +128,11 @@ def summary_tables(stat_dic, precision=2, estimator=True, alpha=True):
     # create bootstrapping parameter summary table
     df_bs = pd.DataFrame(
         data=np.array(
-            [(stat_dic["sample_size"], stat_dic["rep"], (1 - stat_dic["level"]))]),
+            [(stat["sample_size"], stat["rep"], (1 - stat["level"]))]),
         columns=["Sample Size", "Repetition", "Significance Level"])
     
-    if stat_dic["n"] != "auto":
-        df_bs["Samples per bootstrap"] = round(stat_dic["n"], 0)
+    if stat["n"] != "auto":
+        df_bs["Samples per bootstrap"] = round(stat["n"], 0)
 
     # set formatting and caption for table
     bs_params = df_bs.style.format(
