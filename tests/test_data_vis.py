f057605ccd46af0b6c8eff6f44e81d3b30a7f30c
Zack Tang <zacktang113@gmail.com>
2022-01-21 17:27:57 -0800

    add integration test

--- tests/test_data_vis.py
+++ tests/test_data_vis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from straPy.data_vis import histogram_ci_plot
+from straPy.bootstrap import calculate_boot_stats, bootstrap_distribution
 from pytest import raises
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,9 +15,14 @@ def test_histogram_ci_plot():
     None
         The test should pass and no asserts should be displayed.
         
-    6 tests in total
+    7 tests in total
     """
     
+    # test integration with calculate_boot_stats function
+    test_stat = calculate_boot_stats([1, 2, 3, 4], 1000, level=0.95, random_seed=123)
+    assert isinstance(test_stat, dict
+    )
+
     # tests with invalid input type of title
     with raises(TypeError) as e:
         histogram_ci_plot([1, 2, 3, 4, 5, 6, 7], 1000 ,n=100, ci_level=0.95, title = 123)
